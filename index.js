4f5db73be12a4e0da295335f67475df01f577ab9
Venkata Kalyan Sadhanakari <146244535+venkatakalyan07@users.noreply.github.com>
2024-11-01 08:30:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 var len=document.querySelectorAll(".drum").length;
-/*
-for(var i=0;i<len;i++)
-{
-   document.querySelectorAll(".drum")[i].addEventListener("click",function fun(){
-    alert("hello");
-   });
-}
-*/
 for(var i=0;i<len;i++)
 {
    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
@@ -15,40 +7,7 @@ for(var i=0;i<len;i++)
       buttonanimation(buttoninnerhtml);
    })
 }
-/*
-for(var i=0;i<len;i++)
-{
-   document.querySelectorAll("button")[i].addEventListener("click",function(){
-      //this.style.color="orange";
-      buttoninnerhtml=this.innerHTML;
-      switch(buttoninnerhtml) {
-         case 'w':var audio=new Audio('sounds/crash.mp3');
-                  audio.play();
-               break;
-         case 'a':var audio=new Audio('sounds/kick-bass.mp3');
-                  audio.play();
-               break;   
-         case 's':var audio=new Audio('sounds/snare.mp3');
-                  audio.play();
-               break;
-         case 'd':var audio=new Audio('sounds/tom-1.mp3');
-                  audio.play();
-               break;   
-         case 'j':var audio=new Audio('sounds/tom-2.mp3');
-                  audio.play();
-               break;
-         case 'k':var audio=new Audio('sounds/tom-3.mp3');
-                  audio.play();
-               break;   
-         case 'l':var audio=new Audio('sounds/tom-2.mp3');
-                  audio.play();
-               break;         
-         default:console.log(buttoninnerhtml);
-            break;
-      }
-   });
-}
-*/
+
 document.addEventListener("keydown",function(event){
    make_sound(event.key);
    buttonanimation(event.key);
@@ -57,28 +16,28 @@ function make_sound(key)
 {
    switch(key)
    {
-      case 'w':var audio=new Audio('sounds/crash.mp3');
+      case 'w':var audio=new Audio('crash.mp3');
                audio.play();
                break;
-      case 'a':var audio=new Audio('sounds/kick-bass.mp3');
+      case 'a':var audio=new Audio('kick-bass.mp3');
                audio.play();
                break;
-      case 's':var audio=new Audio('sounds/snare.mp3');
+      case 's':var audio=new Audio('snare.mp3');
                audio.play();
                break;
-      case 'd':var audio=new Audio('sounds/tom-1.mp3');
+      case 'd':var audio=new Audio('tom-1.mp3');
                audio.play();
                break;
-      case 'j':var audio=new Audio('sounds/tom-2.mp3');
+      case 'j':var audio=new Audio('tom-2.mp3');
                audio.play();
                break;
-      case 'k':var audio=new Audio('sounds/tom-3.mp3');
+      case 'k':var audio=new Audio('tom-3.mp3');
                audio.play();
                break;
-      case 'l':var audio=new Audio('sounds/tom-4.mp3');
+      case 'l':var audio=new Audio('tom-4.mp3');
                audio.play();
                break;
-      default://console.log(key);
+      default:
       alert(key +" is not valid");
       break;
    }
@@ -89,24 +48,6 @@ function buttonanimation(key)
    activebutton.classList.add("pressed");
    setTimeout(function(){
       activebutton.classList.remove("pressed");
-      
    }, 100);
 }
-/*
-document.addEventListener("keydown",function(event) {
-   //alert("key was pressed");
-   console.log(event);
-})
-   */
 
-/*function audio_play()
-{
-  var audio=new Audio('sounds/tom-3.mp3');
-  audio.play();
-}
-  */
-//document.querySelector("button").addEventListener("click",fun);
-
-//function fun(){
-//    alert("I got clicked");
-//}
